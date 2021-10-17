2b65b0f323b3391459cc755536a5b5c104564d2f
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-10-17 14:58:55 -0500

    Add tailwindcss

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+import './src/styles/tailwind.css';
+import '@fontsource/barlow';
+import '@fontsource/barlow-condensed/400.css';
+import '@fontsource/barlow-condensed/600.css';
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      body: 'Barlow, sans-serif',
+      heading: 'Barlow, sans-serif',
+      display: '"Barlow Condensed", sans-serif',
+    },
+    fontSize: {
+      xs: '1rem',
+      sm: '1.5rem',
+      base: '1.777rem',
+      lg: '2.368rem',
+      xl: '3.157rem',
+      '2xl': '4.209rem',
+      '3xl': '5.61rem',
+      '4xl': '7.478rem',
+    },
+    extend: {
+      colors: {
+        primary: '#0085FF',
+        secondary: '#7421FC',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      cursor: ['disabled'],
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [],
+};
