435a78bab2bb3c09c62354aeba52c855b2297f0e
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-10-24 13:52:54 -0500

    Add fathom analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,16 @@ import '@fontsource/barlow/700.css';
 import '@fontsource/barlow/900.css';
 import '@fontsource/barlow-condensed/400.css';
 import '@fontsource/barlow-condensed/600.css';
+import { Helmet } from 'react-helmet';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <>
+      <Helmet>
+        <script src="https://cdn.usefathom.com/script.js" data-site="XDNCZTKB" defer></script>
+      </Helmet>
+
+      {element}
+    </>
+  );
+};
