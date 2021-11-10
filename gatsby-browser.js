4a3f202ac0a1a6cabe15fbcbb4466b6dc741302c
AndresCorona <andres_corona5@hotmail.com>
2021-11-10 17:19:55 -0600

    Add videoSupport

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,8 +13,8 @@ export const wrapRootElement = ({ element }) => {
     <>
       <Helmet>
         <script src="https://cdn.usefathom.com/script.js" data-site="XDNCZTKB" defer></script>
+        <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
       </Helmet>
-
       {element}
     </>
   );
