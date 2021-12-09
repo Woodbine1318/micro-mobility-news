f6a745318dcb3556a9ce31ba0f3468cfd26a19cd
AndresCorona <andres_corona5@hotmail.com>
2021-12-09 15:41:10 -0600

    Change font to Roboto

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './src/styles/tailwind.css';
-import '@fontsource/barlow/400.css';
-import '@fontsource/barlow/800.css';
-import '@fontsource/barlow/700.css';
-import '@fontsource/barlow/900.css';
-import '@fontsource/barlow-condensed/400.css';
-import '@fontsource/barlow-condensed/600.css';
+import '@fontsource/Roboto/400.css';
+import '@fontsource/Roboto/700.css';
+import '@fontsource/Roboto/900.css';
+import '@fontsource/Roboto-condensed/400.css';
+import '@fontsource/Roboto-condensed/700.css';
 import { Helmet } from 'react-helmet';
 
 export const wrapRootElement = ({ element }) => {
