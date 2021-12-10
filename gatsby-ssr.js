e150cd60f188650e3a5025a4f6e42881c21d3a7e
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-12-10 09:14:58 -0600

    Fix case

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import './src/styles/tailwind.css';
-import '@fontsource/Roboto/400.css';
-import '@fontsource/Roboto/700.css';
-import '@fontsource/Roboto/900.css';
-import '@fontsource/Roboto-condensed/400.css';
-import '@fontsource/Roboto-condensed/700.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+import '@fontsource/roboto-condensed/400.css';
+import '@fontsource/roboto-condensed/700.css';
