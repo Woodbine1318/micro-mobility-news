2426dbb895d5aeab25f8df19d47ba779d1530192
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-12-13 18:39:19 -0600

    Add Google Analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,6 +12,7 @@ export const wrapRootElement = ({ element }) => {
     <>
       <Helmet>
         <script src="https://cdn.usefathom.com/script.js" data-site="XDNCZTKB" defer></script>
+
         <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
       </Helmet>
       {element}
--- gatsby-config.js
+++ gatsby-config.js
@@ -137,5 +137,22 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [process.env.GATSBY_GA_ID],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {},
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          exclude: [],
+        },
+      },
+    },
   ],
 };
